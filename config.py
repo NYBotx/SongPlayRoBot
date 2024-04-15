cc29270cea7e72971a3a9c8ddaea2d3fd5bdfa7a
NYBotx <167092531+NYBotx@users.noreply.github.com>
2024-04-15 18:00:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "19911978"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "e3f5848d4c384af9e6f1f52ca84c19c7" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "7075948687:AAGZymq-DyCiBY4hctkA8a7x3QqdzP4LmTo" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
